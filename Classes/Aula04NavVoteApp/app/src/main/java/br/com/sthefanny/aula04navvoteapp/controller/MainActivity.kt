5ce37e5aa4902606751c58874429a94b0458d60d
Sthefanny Gonzaga <sthefanny.gonzaga@lar.app>
2020-07-04 17:55:51 -0300

    Drawer menu

--- Classes/Aula04NavVoteApp/app/src/main/java/br/com/sthefanny/aula04navvoteapp/controller/MainActivity.kt
+++ Classes/Aula04NavVoteApp/app/src/main/java/br/com/sthefanny/aula04navvoteapp/controller/MainActivity.kt
@@ -2,26 +2,40 @@ package br.com.sthefanny.aula04navvoteapp.controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import br.com.sthefanny.aula04navvoteapp.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        val navView: BottomNavigationView = findViewById(R.id.bottomNavView)
+        val navView: NavigationView = findViewById(R.id.navView)
         val navController = findNavController(R.id.fragNavHost)
-        val appBarConfiguration = AppBarConfiguration(setOf(R.id.voteFragment, R.id.countFragment))
 
+        val drawer: DrawerLayout = findViewById(R.id.layDrawer)
+        appBarConfiguration = AppBarConfiguration(setOf(R.id.voteFragment, R.id.countFragment), drawer)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+
+        val navController = findNavController(R.id.fragNavHost)
+
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
     fun changeTitle(title: String) {
