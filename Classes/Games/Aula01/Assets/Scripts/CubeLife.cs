8ef404625965c4e3bec8ada58388b6837a334124
Sthefanny Gonzaga <sthefannygonzaga@gmail.com>
2021-04-25 10:21:48 -0300

    Adding game class

---
+++ Classes/Games/Aula01/Assets/Scripts/CubeLife.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class CubeLife : MonoBehaviour
+{
+    int touchCount = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.GetComponent<MeshRenderer>().material.color = Color.gray;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("ball"))
+        {
+            var saturation = 0f;
+
+            switch (touchCount)
+            {
+                case 0:
+                    saturation = 0.3f;
+                    break;
+                case 1:
+                    saturation = 0.6f;
+                    break;
+                case 2:
+                    saturation = 1f;
+                    break;
+                default:
+                    return;
+            }
+            touchCount++;
+            this.transform.GetComponent<MeshRenderer>().material.color = Color.HSVToRGB(0, saturation, 1f);
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (touchCount == 3)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
